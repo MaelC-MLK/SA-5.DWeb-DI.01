dec08b7a03db32fdc5ce36b9448afe66e889134b
Mael C. / Malek <74120418+MalekCrapule@users.noreply.github.com>
2024-10-01 18:37:23 +0200

    Changement BTN Delete Scene

--- main.js
+++ main.js
@@ -213,7 +213,6 @@ document.addEventListener("DOMContentLoaded", function () {
           id: sceneId,
           name: `Scène ${scenes.length + 1}`,
           src: e.target.result,
-          fileName: file.name,
         });
 
         const option = document.createElement("option");
@@ -226,7 +225,6 @@ document.addEventListener("DOMContentLoaded", function () {
         displayScene(sceneId);
         editSceneForm.classList.remove("hidden");
         sceneNameInput.value = `Scène ${scenes.length}`;
-        fileName.textContent = file.name;
       };
       reader.readAsDataURL(file);
     }
@@ -623,8 +621,9 @@ function deleteScene(sceneId) {
     }
     sceneDropdown.value = "";
     editSceneForm.classList.add("hidden");
-    fileName.textContent = "";
     displayDefaultScene();
+    updateSceneDropdown();
+    checkScenesAndToggleSubMenu();
   }
 }
 // Fonction pour gérer la sélection des tags
@@ -696,12 +695,16 @@ export function checkScenesAndToggleSubMenu() {
   const allScenes = document.querySelectorAll("a-scene");
   const subMenuCreateTag = document.getElementById("subMenuCreateTag");
   const exportSaveBtn = document.getElementById("exportSaveBtn");
-  if (allScenes.length > 0) {
+  const deleteSceneBtn = document.getElementById("deleteSceneBtn");
+
+  if (allScenes.length > 1) { // Vérifie s'il y a plus d'une scène
     subMenuCreateTag.classList.remove("hidden");
     exportSaveBtn.classList.remove("hidden");
+    deleteSceneBtn.disabled = false; // Active le bouton
   } else {
     subMenuCreateTag.classList.add("hidden");
     exportSaveBtn.classList.add("hidden");
+    deleteSceneBtn.disabled = true; // Désactive le bouton
   }
 }
 
